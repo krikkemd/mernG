c915a6a218c390730a1570a1cde4ee65410a6e59
Mickey <krikkemd@gmail.com>
2021-07-23 15:25:20 +0200

    DeleteButton can now delete Posts and Comments

--- frontend/src/components/DeleteButton.js
+++ frontend/src/components/DeleteButton.js
@@ -2,35 +2,47 @@ import React, { useState } from 'react';
 import { Button, Confirm } from 'semantic-ui-react';
 import { useMutation } from '@apollo/client';
 import { DELETE_POST, GET_POSTS } from '../graphql/posts';
+import { DELETE_COMMENT } from '../graphql/comments';
 
 import { useParams, useHistory } from 'react-router-dom';
 
 const DeleteButton = ({ post, comment }) => {
+  console.log(comment);
   const [confirmOpen, setConfirmOpen] = useState(false);
+  // const [mutation, setMutation] = useState('');
+  let mutation;
+
+  // if there is a comment(Id) passed in, delete the comment. Else delete the post
+  comment ? (mutation = DELETE_COMMENT) : (mutation = DELETE_POST);
 
   const history = useHistory();
   const { postId } = useParams(); // url parameter, if present, we are on the SinglePost page. We need to redirect when we delete. see onCompleted()
 
-  const [deletePost, { client }] = useMutation(DELETE_POST, {
+  const [deletePostOrComment, { client }] = useMutation(mutation, {
     update() {
-      const { getPosts } = client.readQuery({ query: GET_POSTS });
+      // Only when a post is deleted, splicePosts
+      if (!comment) {
+        const { getPosts } = client.readQuery({ query: GET_POSTS });
 
-      const splicedPosts = getPosts.filter(cachedPost => cachedPost.id !== post.id);
+        const splicedPosts = getPosts.filter(cachedPost => cachedPost.id !== post.id);
 
-      client.writeQuery({
-        query: GET_POSTS,
-        data: {
-          getPosts: [...splicedPosts],
-        },
-      });
+        client.writeQuery({
+          query: GET_POSTS,
+          data: {
+            getPosts: [...splicedPosts],
+          },
+        });
+      }
     },
 
     onCompleted() {
       setConfirmOpen(false);
 
       // When on the SinglePostPage, redirect to the homepage when the post is deleted
-      if (history.location.pathname === `/posts/${postId}`) {
-        history.push('/');
+      if (!comment) {
+        if (history.location.pathname === `/posts/${postId}`) {
+          history.push('/');
+        }
       }
     },
 
@@ -40,6 +52,7 @@ const DeleteButton = ({ post, comment }) => {
 
     variables: {
       postId: post.id,
+      commentId: comment?.id,
     },
   });
 
@@ -55,7 +68,11 @@ const DeleteButton = ({ post, comment }) => {
       />
 
       {/* Confirm modal */}
-      <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePost} />
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePostOrComment}
+      />
     </>
   );
 };
